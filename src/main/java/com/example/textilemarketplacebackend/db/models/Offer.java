d4cb074e32ca14f7f277f220cfa6b89a9b07a4a8
Dawid141 <chudaowca@gmail.com>
2024-11-19 13:29:38 +0100

    Data base updates

--- src/main/java/com/example/textilemarketplacebackend/db/models/Offer.java
+++ src/main/java/com/example/textilemarketplacebackend/db/models/Offer.java
@@ -1,33 +1,83 @@
 package com.example.textilemarketplacebackend.db.models;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-import java.math.BigDecimal;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
-@Table(name = "offers")
+@Table(name = "offer")
 public class Offer {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer offerId;
+    @Column(name = "id", nullable = false)
+    private Long id;
 
-    @ManyToOne
+    @Column(name = "product_name", nullable = false)
+    private String productName;
+
+    @Column(name = "short_description", nullable = false)
+    private String shortDescription;
+
+    @Column(name = "long_description", nullable = false, length = 1024)
+    private String longDescription;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+
+    @ManyToOne(optional = false)
     @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+    private LocalUser user;
 
-    @Column(nullable = false)
-    private Integer productQuantity;
+    public LocalUser getUser() {
+        return user;
+    }
 
-    @Column(nullable = false)
-    private BigDecimal productPrice;  // Użycie BigDecimal zamiast Double
+    public void setUser(LocalUser user) {
+        this.user = user;
+    }
 
-    @Column(columnDefinition = "TEXT")
-    private String productDescription;
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) {this.id = id; }
 }
