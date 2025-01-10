b0c640c61fd8e91d2bfe53907c27a6990c79e595
JakubOkrzesik <s221507@student.uek.krakow.pl>
2025-01-10 17:00:45 +0100

    Handling amazon secrets added

--- src/main/java/com/example/textilemarketplacebackend/products/config/StorageConfig.java
+++ src/main/java/com/example/textilemarketplacebackend/products/config/StorageConfig.java
@@ -5,26 +5,24 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.example.textilemarketplacebackend.global.services.EnvService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class StorageConfig {
 
-    @Value("${cloud.aws.credentials.access-key}")
-    private String access_key;
-    @Value("${cloud.aws.credentials.secret_key}")
-    private String secret_key;
-    @Value("${cloud.aws.region.static}")
-    private String region;
+    private final EnvService envService;
 
     @Bean
     public AmazonS3 s3Client() {
-        AWSCredentials credentials = new BasicAWSCredentials(access_key, secret_key);
+        AWSCredentials credentials = new BasicAWSCredentials(envService.getAWS_ACCESS_KEY(), envService.getAWS_SECRET_KEY());
         return AmazonS3ClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(region)
+                .withRegion(envService.getAWS_REGION())
                 .build();
     }
 }
